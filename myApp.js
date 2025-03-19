4f58ca1e00cf12a9133485621303041a7813a304
DiegoArayaT <nachox341@gmail.com>
2025-03-19 12:50:09 +0000

    Install and Set Up Mongoose

--- myApp.js
+++ myApp.js
@@ -1,7 +1,12 @@
 require('dotenv').config();
+const mongoose = require('mongoose')
+const uri = process.env.MONGO_URI
 
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 let Person;
+
+
 
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
